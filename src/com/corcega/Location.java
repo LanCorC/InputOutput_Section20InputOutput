e294729c22713c1675a29222e60c9b9d5e94b325
Lance <lanc3.corcega@gmail.com>
2023-06-18 19:15:07 +1200

    Section 20.312(JavaFX; Introduction to I/O) - setting up for next area in lesson.

---
+++ src/com/corcega/Location.java
@@ -0,0 +1,43 @@
+package com.corcega;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description,
+                    Map<String, Integer> exits) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<>();
+        if (exits != null) {
+            for (String key : exits.keySet()) { //Checks for fail Integer
+                if (exits.get(key) != null) {
+                    this.exits.put(key, exits.get(key));
+                }
+            }
+        }
+        this.exits.put("Q", 0);
+    }
+
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+
+
+}
