882bd90e0365aa56e508c3e8a0a85045be206f67
Lance <lanc3.corcega@gmail.com>
2023-06-29 16:51:59 +1200

    Section 20.323(JavaFX; Finish Object I/O and RandomAccessFile class): theory heavy introduction to RandomAccessFile, about index and data (record) sections, filepointers, offsets

--- src/com/corcega/Locations.java
+++ src/com/corcega/Locations.java
@@ -7,23 +7,6 @@ public class Locations implements Map<Integer, Location>  {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args)  throws IOException {
-//        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))) {
-//            for(Location location : locations.values()) {
-//                locFile.writeInt(location.getLocationID());
-//                locFile.writeUTF(location.getDescription());
-//                System.out.println("Writing location " + location.getLocationID() + " : " + location.getDescription());
-//                System.out.println("Writing " + (location.getExits().size() -1) + " exits.");
-//                locFile.writeInt(location.getExits().size() - 1);
-//                for(String direction : location.getExits().keySet()) {
-//                    if (!direction.equalsIgnoreCase("Q")) {
-//                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
-//                        locFile.writeUTF(direction);
-//                        locFile.writeInt(location.getExits().get(direction));
-//                    }
-//                }
-//            }
-//        }
-
         try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))) {
             for(Location location : locations.values()) {
                 locFile.writeObject(location);
@@ -32,13 +15,18 @@ public class Locations implements Map<Integer, Location>  {
 
     }
 
+    //1. first four bytes - # of locations (0-3 byte)
+    //2. next 4 - offset of locations section (4-7 byte)
+    //3. next section - contain the index. 1692 bytes long (8-1699)
+    //4. final - contain location records - (1700 byte +)
+
     static {
         try(ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))) {
             boolean eof = false;
-            while(!eof) {
+            while (!eof) {
                 try {
                     Location location = (Location) locFile.readObject();
-                    System.out.println("Read location " + location.getLocationID() + " : "  + location.getDescription());
+                    System.out.println("Read location " + location.getLocationID() + " : " + location.getDescription());
                     System.out.println("Found " + location.getExits().size() + " exits");
 
                     locations.put(location.getLocationID(), location);
@@ -46,54 +34,13 @@ public class Locations implements Map<Integer, Location>  {
                     eof = true;
                 }
             }
-//            while(!eof) {
-//                try {
-//                    Map<String, Integer> exits = new LinkedHashMap<>();
-//                    int locID = locFile.readInt();
-//                    String description = locFile.readUTF();
-//                    int numExits = locFile.readInt();
-//                    System.out.println("Read location " + locID + " : " + description);
-//                    System.out.println("Found " + numExits + " exits");
-//                    for(int i = 0; i<numExits; i++) {
-//                        String direction = locFile.readUTF();
-//                        int destination = locFile.readInt();
-//                        exits.put(direction, destination);
-//                        System.out.println("\t\t" + direction + "," + destination);
-//                    }
-//                    locations.put(locID, new Location(locID, description, exits));
-//                } catch(EOFException e) {
-//                    eof = true;
-//                }
-//            }
-
+        } catch(InvalidClassException e) {
+            System.out.println("InvalidClassException " + e.getMessage());
         } catch(IOException io) {
             System.out.println("IO Exception" + io.getMessage());
         } catch(ClassNotFoundException e) {
             System.out.println("ClassNotFoundException " + e.getMessage());
         }
-//        try(BufferedReader locRead = new BufferedReader(new FileReader("locations_big.txt"));
-//            BufferedReader dirRead = new BufferedReader(new FileReader("directions_big.txt"))) {
-//            String input;
-//            //Read locations
-//            while((input = locRead.readLine()) != null) {
-//                String[] data = input.split(",", 2);
-//                int loc = Integer.parseInt(data[0]);
-//                String des = data[1];
-//                Map<String, Integer> tempExit = new LinkedHashMap<>();
-//                locations.put(loc, new Location(loc, des, tempExit));
-//            }
-//            //Read exits
-//            while((input = dirRead.readLine()) != null) {
-//                String[] data = input.split(",");
-//                int loc = Integer.parseInt(data[0]);
-//                String dir = data[1];
-//                int des = Integer.parseInt(data[2]);
-//                Location location = locations.get(loc);
-//                location.addExit(dir, des);
-//            }
-//        } catch(IOException e) {
-//            e.printStackTrace();
-//        }
     }
     @Override
     public int size() {
